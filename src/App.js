6252dbb95413ef3e3e9a44ac784473014a2eb17c
Alireza Karimi <alireza.karimi.67@gmail.com>
2020-07-16 23:32:03 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter} from 'react-router-dom'
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>TEST</h1>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
