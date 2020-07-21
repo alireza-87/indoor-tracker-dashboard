51cf02e66469318e9e972db4360ada2e96d19556
Alireza Karimi <alireza.karimi.67@gmail.com>
2020-07-21 17:41:31 +0200

    add total count to redux

--- src/store/reducers/roomsReducer.js
+++ src/store/reducers/roomsReducer.js
@@ -1,19 +1,37 @@
 import * as actionType from '../actionType'
 
-const roomReducer = (state = { roomList : [] }, action) => {
+const roomReducer = (state = {totalCurrentOccupide:0, totalCount:0,roomList : [] }, action) => {
   console.log('roomReducer actio  > ',action);
   console.log('roomReducer state > ',state);
+  
+  const totalCount = () =>{
+    let c=0
+    state.roomList.forEach(item => {c=c+item.capacity})
+    return c
+  }
+  
+  const totalCurrentOccupide = () =>{
+    let c=0
+    if(state.roomList)
+      state.roomList.forEach(item => {item.counter? c=c+item.counter:c=c+0})
+    return c
+  }
 
   switch (action.type) {
     case actionType.ROOM_LIST:
       return {
         ...state, 
+        totalCount:totalCount(),
+        totalCurrentOccupide:totalCurrentOccupide(),
         roomList: [ ...action.payload.rooms]
       };
     case actionType.ROOM_COUNT:
       
       return {
         ...state, 
+        totalCount:totalCount(),
+        totalCurrentOccupide:totalCurrentOccupide(),
+
         roomList: state.roomList.map(item => item.floor === action.payload.rooms.floor && item.room === action.payload.rooms.room ? {...item,counter:action.payload.rooms.count} : {...item} )
       } 
     default:
