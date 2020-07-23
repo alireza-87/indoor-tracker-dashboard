e8b94d3ebc2c8e93ab6619321cb657454bcf2a7d
Alireza Karimi <alireza.karimi.67@gmail.com>
2020-07-23 20:13:14 +0200

    chech auth

--- src/components/layout/SignedInLink.js
+++ src/components/layout/SignedInLink.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import { NavLink} from 'react-router-dom'
-const SignedInLink = () => {
+import {connect} from 'react-redux'
 
+const SignedInLink = (props) => {
+    const level= (props.loginData && props.loginData.message === "success" && props.loginData.result.rule === 'admin')?1:0
     return(
         <ul className="right">
-            <li>
-                <NavLink to='/addroom'>Add Room</NavLink>
-            </li>
-            <li>
-                <NavLink to='/addperson'>Add User</NavLink>
-            </li>
-            <li>
-                <NavLink to='/person'>User List</NavLink>
-            </li>
+            { level === 1 ? <li><NavLink to='/addroom'>Add Room</NavLink></li>:null}
+            { level === 1 ? <li><NavLink to='/addperson'>Add User</NavLink></li>:null}
+            { level === 1 ? <li><NavLink to='/person'>User List</NavLink></li>:null}
             <li>
                 <NavLink to='/'>Logout</NavLink>
             </li>
@@ -25,4 +21,10 @@ const SignedInLink = () => {
 
 }
 
-export default SignedInLink
+const mapStateToProps = (state) =>{
+    return({
+        loginData:state.auth.login
+    })
+}
+
+export default connect(mapStateToProps)(SignedInLink)
