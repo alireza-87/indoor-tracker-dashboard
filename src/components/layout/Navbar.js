ab9e91fd96f3412a759d9de6f848f6fab7261f57
Alireza Karimi <alireza.karimi.67@gmail.com>
2020-07-23 19:03:21 +0200

    fix navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,17 +2,25 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import  SignedInLink from './SignedInLink'
 import  SignedOutLink from './SignedOutLink'
+import {connect} from 'react-redux'
 
-const Navbar =() => {
+const Navbar =(props) => {
+    const link = (props.loginData && props.loginData.message === "success") ?<SignedInLink /> :<SignedOutLink />
     return(
         <nav className="nav-wrapper blue-grey darken-4 z-depth-2">
             <div className="container">
                 <Link to='/' className='brand-logo'>Indoor Tracker</Link>
-                <SignedInLink />
-                <SignedOutLink />
+                {link}
             </div>
         </nav>
     )
 }
 
-export default Navbar
+
+const mapStateToProps = (state) =>{
+    return({
+        loginData:state.auth.login
+    })
+}
+
+export default connect(mapStateToProps)(Navbar)
