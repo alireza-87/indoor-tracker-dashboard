436d32f812aeefcb14a8d5f16c2f5d85508e8ef7
Alireza Karimi <alireza.karimi.67@gmail.com>
2020-07-24 23:54:49 +0200

    css

--- src/components/monitoring/ServerDiagram.js
+++ src/components/monitoring/ServerDiagram.js
@@ -10,17 +10,23 @@ const ServerDiagram = ({totalCount,totalCurrentOccupide}) =>{
     console.log(data)
     return(
         <div className="card z-depth-2 server-summery">
-        <div className="card-content grey-text text-darker-3">
-                <span className="card-title">
-                <XYPlot height={300} width= {300}>
-                <VerticalGridLines />
-                <HorizontalGridLines />
-                <XAxis />
-                <YAxis />
-                <LineSeries height={300} width={300} data={[...data,...totalCurrentOccupide.map(item => { return {x: index++, y: item} })]} />
-                </XYPlot>
-                </span>
-        </div>
+            <div>
+                <div className="card-title">
+                    <h6 className="card-title gray-text darken-4">Attendees change</h6>
+                </div>
+                <div className="div-seprator"></div>
+                <div className="grey-text text-darker-3">
+                    <span className="card-title">
+                    <XYPlot height={300} width= {400}>
+                    <XAxis />
+                    <YAxis title="Number of Attendees"/>
+                    <VerticalGridLines />
+                    <HorizontalGridLines />
+                    <LineSeries color="#f9a825"  style={{strokeWidth: 3}} height={300} width={300} data={[...data,...totalCurrentOccupide.map(item => { return {x: index++, y: item} })]} />
+                    </XYPlot>
+                    </span>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/room/RoomSummery.js
+++ src/components/room/RoomSummery.js
@@ -4,15 +4,17 @@ const RoomSummery = ({room}) => {
     return(
         <div className="col s12 m6 l4">
         <div className="card z-depth-2 room-summery">
+            <div className="card-title">
+                <h6 className="card-title gray-text darken-4">{room.name}</h6>
+            </div>
             <div className="card-content grey-text text-darker-3">
-                <span className="card-title">
-                    {room.name}
-                </span>
                 <h6>Floor : {room.floor}</h6>
                 <h6>Room :  {room.room}</h6>
                 <h6>Current Occupied : {room.counter}</h6>
                 <h6>Capacity : {room.capacity}</h6>
-                <h6>Is Enable :  {room.isenable}</h6>
+            </div>
+            <div className="div-seprator"></div>
+            <div className="card-action">
                 <NavLink to={`/person/${room.floor}/${room.room}`}>Detail...</NavLink>
             </div>
         </div>
