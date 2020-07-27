8c8a5fe7ca4baddb79ff6550b991fdd7224c9a6c
AlirezaKarimi <alireza.karimi.67@gmail.com>
2020-07-27 15:25:23 +0200

    fix

--- src/components/logsmonitoring/ActivityMonitoringSummery.js
+++ src/components/logsmonitoring/ActivityMonitoringSummery.js
@@ -1,26 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class ActivityMonitoringSummery extends Component {
-    constructor(props){
-        super(props)
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        const {activitylog}=this.props
-        if(activitylog.user){
-            return (
-                <div >
-                    <p > {activitylog.user.name} {activitylog.action === "entertoroom"?" Enter to ":" Exit from "}floor {activitylog.floor} room {activitylog.room}</p>               
-                </div>
-            )
-        }else{
-            return (
-                <div >
-                    <p > {activitylog.type === "roomoverload"?" Room over :  ":""} floor : {activitylog.floor} room: {activitylog.room}</p>               
-                </div>
-            )
-        }
+  render() {
+    const { activitylog } = this.props;
+    if (activitylog.user) {
+      console.log("RRRRR ", activitylog.time);
+      let a = new Date(activitylog.time);
+      var hours = a.getHours();
+      console.log("RRRRR ", hours);
+      let current_datetime = new Date(activitylog.time);
+      let formatted =
+        current_datetime.getFullYear() +
+        "-" +
+        (current_datetime.getMonth() + 1) +
+        "-" +
+        current_datetime.getDate() +
+        " " +
+        current_datetime.getHours() +
+        ":" +
+        current_datetime.getMinutes() +
+        ":" +
+        current_datetime.getSeconds();
+      return (
+        <div>
+          <p>
+            {formatted} {activitylog.user.name}{" "}
+            {activitylog.action === "entertoroom"
+              ? " Enter to "
+              : " Exit from "}
+            floor {activitylog.floor} room {activitylog.room}
+          </p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <p>
+            {" "}
+            {activitylog.type === "roomoverload" ? " Room over :  " : ""} floor
+            : {activitylog.floor} room: {activitylog.room}
+          </p>
+        </div>
+      );
     }
+  }
 }
 
-export default ActivityMonitoringSummery
+export default ActivityMonitoringSummery;
